21f47513fb7f32f661ce156f6772cbbad4672752
Abdullah Memon <58343555+azterizm@users.noreply.github.com>
2020-10-15 16:22:01 +0500

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -1,28 +1,6 @@
 const admin = require('firebase-admin');
 const { auth } = admin;
 
-// module.exports = () => async (req, res, next) => {
-//   // initialize the firebase application
-
-//   try {
-//     // retrieve the authorization header
-//     const { authorization } = req.headers;
-//     const { 1: idToken } = authorization.split('Bearer ');
-
-//     // verify the user id token
-//     const decodedToken = await auth().verifyIdToken(idToken);
-
-//     // set it on the request variable so we can use on the next routes
-//     req.locals = { user: decodedToken };
-
-//     return next();
-//   } catch (error) {
-//     console.log(error);
-
-//     return res.sendStatus(401);
-//   }
-// };
-
 module.exports = () => async (req, res, next) => {
   try {
     const { authorization } = req.headers;
